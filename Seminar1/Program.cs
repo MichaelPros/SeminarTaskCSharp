061d8db142a0313cc96b0156a9413f69e6042832
MichaelPros <r-0221@inbox.ru>
2023-09-07 15:22:34 +1000

    ДЗ по семинару 2

--- Seminar1/Program.cs
+++ Seminar1/Program.cs
@@ -106,3 +106,79 @@ a = -9 b = -3 -> max = -3*/
 //         index = index + 1;
 //     }
 // }
+
+
+/*
+Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+// Console.WriteLine("Enter number; ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// if (num > 99 && num < 1000)
+// {
+//     int num1 = num / 10;
+//     int num2 = num1 % 10;
+//     Console.WriteLine(num2);
+// }
+// else
+// {
+//     Console.WriteLine("Incorrect number");
+// }
+
+/*
+Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6
+*/
+
+// Console.WriteLine("Enter number");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// if (num < 100)
+// {
+//     Console.WriteLine("третьей цифры нет");
+// }
+// else if (num < 1000)
+// {
+//     int num1 = num % 10;
+//     Console.WriteLine(num1);
+// }
+// else
+// {
+//     while (num >= 1000)
+//     {
+//         num = num / 10;
+//     }
+//     Console.WriteLine(num % 10);
+// }
+
+/*
+Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+6 -> да
+7 -> да
+1 -> нет
+*/
+
+// Console.WriteLine("Enter number day of week");
+// int num = Convert.ToInt32(Console.ReadLine());
+// if (num < 1 || num > 7)
+// {
+//     Console.WriteLine("Enter correkt day of week");
+// }
+// else
+// {
+//     if(num == 6 || num == 7)
+//     {
+//         Console.WriteLine("Yes");
+//     }
+//     else
+//     {
+//         Console.WriteLine("No");
+//     }
+
+
+// }
+
